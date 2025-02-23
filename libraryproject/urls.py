ec678d3f6db32294cc85d2988350102d301f6d48
abdulkarim-alresheedi <akmrsd12@gmail.com>
2025-02-23 23:22:23 +0300

    Lab3

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include, path
+
+import apps.bookmodule.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', include("apps.bookmodule.urls")),
+    path('users/', include("apps.usermodule.urls")),
 ]
