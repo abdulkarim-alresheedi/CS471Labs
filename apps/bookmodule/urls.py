a3345442b637b994dd8342ecd5477430908ca4e6
abdulkarim-alresheedi <akmrsd12@gmail.com>
2025-03-13 06:01:20 +0300

    Lab6

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('html5/text/formatting/', views.html5_text_formatting, name="books.text_formatting"),
     path('html5/listing/', views.html5_listing, name="books.listing"),
     path('html5/tables/', views.html5_tables, name="books.tables"),
+    path('search/', views.search_books, name='search_books'),
 
 
  ] 
